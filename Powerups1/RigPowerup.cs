c25dd640c311806510a17db042591f2df2812084
Austin Cocoli <66973546+OliAe@users.noreply.github.com>
2020-06-26 12:30:15 -0400

    Add files via upload

---
+++ Powerups1/RigPowerup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigPowerup : MonoBehaviour
+{
+    public int jumps = 1;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Pickup(other);
+        }
+
+        void Pickup(Collider player)
+        {
+            RIGbodyMOVE leap = player.GetComponent<RIGbodyMOVE>();
+            leap.MAXJUMP += jumps;
+            
+
+            Destroy(gameObject);
+        }
+    }
+}
