b1622417f4ee7a911fa3019a7bac825e9789bac1
Austin Cocoli <66973546+OliAe@users.noreply.github.com>
2020-06-27 15:30:24 -0400

    Add files via upload

---
+++ Misc/Finish.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour
+{
+    private int nextSceneToLoad;
+
+    private Scene scene;
+    void Start()
+    {
+        nextSceneToLoad = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag =="Player" )
+        {
+            SceneManager.LoadScene(nextSceneToLoad);
+        }
+    }
+
+}
+
---
+++ Misc/Orbit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public GameObject meteor;
+    public float speed;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        OrbitAround ();
+    }
+
+    void OrbitAround()
+    {
+        transform.RotateAround(meteor.transform.position, Vector3.up, speed * Time.deltaTime);
+    }
+}
