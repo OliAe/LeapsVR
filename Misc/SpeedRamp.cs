581dd92c12c8ab4f0a41aaf5a64a2a72bb1cb264
Austin Cocoli <66973546+OliAe@users.noreply.github.com>
2020-06-29 16:59:36 -0400

    Add files via upload

---
+++ Misc/SpeedRamp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedRamp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float explosiveForce = 100000f;
+    public float zaxisForce = 10000f;
+    public int force = 100000;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Collider[] cols = Physics.OverlapSphere(this.transform.position, force);
+            for (int i = 0; i < cols.Length; i++)
+            {
+                Rigidbody rb = cols[i].GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+                    rb.AddForce(Vector3.forward * force, ForceMode.Force);
+                }
+            }
+
+        }
+
+    }
+}
+
