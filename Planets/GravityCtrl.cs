ae8a9a4b2c07c6131770fec9375f1b629f1bba5e
Austin Cocoli <66973546+OliAe@users.noreply.github.com>
2020-07-03 10:29:46 -0400

    Add files via upload

---
+++ Planets/GravityCtrl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityCtrl : MonoBehaviour
+{
+    public GravityOrbit Gravity;
+    private Rigidbody rb;
+
+    public float number = 10f;
+
+    public float rotationSpeed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Gravity) // if there is a set planet to orbit
+        {
+            Vector3 gravityUp = Vector3.zero;
+
+            gravityUp = (transform.position - Gravity.transform.position).normalized;
+
+            Vector3 localUp = transform.up;
+
+            Quaternion targetrotation = Quaternion.FromToRotation(localUp, gravityUp) * transform.rotation;
+
+            transform.up = Vector3.Lerp(transform.up, gravityUp, rotationSpeed * Time.deltaTime);
+
+            rb.AddForce((-gravityUp * number) * rb.mass);
+        }
+    }
+}
---
+++ Planets/GravityOrbit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityOrbit : MonoBehaviour
+{
+    public float Gravity;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<GravityCtrl>())
+        {
+            //if this object has a gravity script, set his as the planet 
+            other.GetComponent<GravityCtrl>().Gravity = this.GetComponent<GravityOrbit>();
+        }
+    }
+}
